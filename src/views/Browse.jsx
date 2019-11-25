5d66057480d9eb6740b8c08b8f6295a46b2ca3c6
Jeremy Rose <jeremy.r.rose@gmail.com>
2019-11-25 09:01:41 -0500

    updated readme and added public meta with screenshot

--- src/views/Browse.jsx
+++ src/views/Browse.jsx
@@ -29,7 +29,7 @@ export class Browse extends React.Component {
     render() {
         this.state.browseList[0] || this.getBrowseList();
         const browseList = this.state.browseList;
-        const badges = browseList.map(user => <UserBadge name={this.props.data[user].first + this.props.data[user].last} avatar={this.props.data[user].avatar} cityState={this.props.data[user].city + ', ' + this.props.data[user].state} user={user} />);
+        const badges = browseList.map(user => <UserBadge name={this.props.data[user].first + ' ' + this.props.data[user].last} avatar={this.props.data[user].avatar} cityState={this.props.data[user].city + ', ' + this.props.data[user].state} user={user} />);
 
         return <main class="browse">{badges}</main>;
     }
